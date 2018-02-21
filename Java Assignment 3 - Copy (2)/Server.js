a01560f293bbbf9970dc65d84befcd628072950b
kranthi467 <kkr.kranthikumar@gmail.com>
2018-02-21 20:00:12 +0530

    WebshareApi

--- Java Assignment 3 - Copy (2)/Server.js
+++ Java Assignment 3 - Copy (2)/Server.js
@@ -1,14 +1,14 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const router = jsonServer.router('db.json', 'routes.json');
+const middlewares = jsonServer.defaults('route');
 
 server.use(jsonServer.bodyParser)
 server.use(middlewares);
 server.use((req, res, next) => {
-  console.log(req.method); 
-  if (isValid(req)) { 
-    next() 
+  console.log(req.method);
+  if (isValid(req)) {
+    next()
   } else {
     res.sendStatus(401)
   }
@@ -19,13 +19,13 @@ server.listen(3000, () => {
 });
 
 var isValid = (req) => {
-    if(req.method == 'GET' || req.method == 'DELETE'){
-      return true;
-    }
-    if(req.body.detail.length > 100){
-      return false;
-    }
-    else{
-      return true;
-    }
+  if (req.method == 'GET' || req.method == 'DELETE') {
+    return true;
+  }
+  if (req.body.detail.length > 100) {
+    return false;
+  }
+  else {
+    return true;
+  }
 }
